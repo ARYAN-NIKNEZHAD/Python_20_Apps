67a2df323dec2b9c04be744b45518e6b4fc798ec
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-02 09:38:26 +0430

    Add-New_thing

--- App_1/main.py
+++ App_1/main.py
@@ -1,3 +1,10 @@
-user_prompt = "Enter a todo"
-user_text = input(user_prompt)
-print(user_text)
+user_prompt = "Enter a todo: "
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+
+todos = [todo1, todo2, todo3, "Hello"]
+print(todos)
+print(type(todo1))
+print(type(todos))
