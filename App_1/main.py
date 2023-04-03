c41cb9086acec200ed8f6146b949f27cc616806b
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-03 10:39:53 +0430

    Add-new_changes

--- App_1/main.py
+++ App_1/main.py
@@ -1,10 +1,7 @@
 user_prompt = "Enter a todo: "
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
 
+while True:
+    todo = input(user_prompt)
+    print(todo)
+    print("Next....")
 
-todos = [todo1, todo2, todo3, "Hello"]
-print(todos)
-print(type(todo1))
-print(type(todos))
