52055f958c650ffcad8b89132cc86d37874e2557
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-06 11:30:59 +0430

    Add-changes

--- App_1/main.py
+++ App_1/main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip()
     
     match user_action:
@@ -12,6 +12,12 @@ while True:
             for item in todos:
                 item = item.title()
                 print(item)
+        case "edit":
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter a new todo: ")
+
+            todos[number] = new_todo
         case "exit":
             break
 
