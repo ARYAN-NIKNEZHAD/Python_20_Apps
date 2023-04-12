3d2d57e8c2e82ad162aea6f2c42cc3f1c2ed48f9
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-12 09:31:48 +0430

    Add-changes

--- App_1/main.py
+++ App_1/main.py
@@ -1,22 +1,4 @@
-def get_todos(file_path="files/todos"):
-    """
-    Read a text file and return the list of todo items
-    :param file_path
-    """
-    with open(file_path, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, file_path="files/todos"):
-    """
-    Write the todo items list in the next file
-    :param todos_arg:
-    :param file_path:
-    """
-    with open(file_path, "w") as file_local:
-        file_local.writelines(todos_arg)
-
+from functions import get_todos, write_todos
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
