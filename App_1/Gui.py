a97fe1bb34fe2459a3579d554a15d38ec97a7bb6
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-13 08:30:13 +0430

    make_Add-Button

--- App_1/Gui.py
+++ App_1/Gui.py
@@ -7,8 +7,18 @@ add_button = Psg.Button("Add")
 window = Psg.Window("My To-do App",
                     layout=[[label], [input_box], [add_button]],
                     font=("Helvetica", 20))
-event = window.read()
-print(event)
+
+while True:
+
+    event, value = window.read()
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = value["todo"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case Psg.WIN_CLOSED:
+            break
 
 window.close()
 
