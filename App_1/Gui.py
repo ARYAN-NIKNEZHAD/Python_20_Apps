aadfd90138c6fd93dee4d46af77321bc1d311088
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-13 08:09:41 +0430

    Add-changes

--- App_1/Gui.py
+++ App_1/Gui.py
@@ -4,7 +4,7 @@ import PySimpleGUI as Psg
 label = Psg.Text("Type in a to-do")
 input_box = Psg.InputText(tooltip="Enter todo")
 add_button = Psg.Button("Add")
-window = Psg.Window("My To-do App", layout=[[label], [input_box, add_button]])
+window = Psg.Window("My To-do App", layout=[[label], [input_box], [add_button]])
 window.read()
 window.close()
 
