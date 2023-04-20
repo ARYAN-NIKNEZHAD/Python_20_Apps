397778b63f0a320820e5b03a3e2abfd6ff77d16e
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2023-04-20 10:55:12 +0430

    Implement_Complete_Exit_Button

--- App_1/Gui.py
+++ App_1/Gui.py
@@ -8,8 +8,13 @@ add_button = Psg.Button("Add")
 list_box = Psg.Listbox(values=functions.get_todos(), key="todos",
                        enable_events=True, size=(45, 10))
 edit_button = Psg.Button("Edit")
+complete_button = Psg.Button("Complete")
+exit_button = Psg.Button("Exit")
 window = Psg.Window("My To-do App",
-                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                    layout=[[label],
+                            [input_box, add_button],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]],
                     font=("Helvetica", 20))
 
 while True:
@@ -31,6 +36,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window["todos"].update(values=todos)
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case "todos":
             window["todo"].update(value=values["todos"][0])
         case Psg.WIN_CLOSED:
